305fc9e60b7b2626053349c38967b7977cad770f
Petr Hodina <hodinapetr46@gmail.com>
2018-04-12 20:13:01 +0200

    Add error-chain & git2 as dependencies. Init repo & check for dirty index. Method to commit index.

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,80 @@
+#[macro_use]
+extern crate error_chain;
+extern crate git2;
+
+use std::path::Path;
+use git2::{Commit, ObjectType, Oid, Repository, RepositoryState, Signature};
+
+error_chain!{
+    foreign_links{
+        Git(::git2::Error);
+    }
+}
 
 pub struct Backup {
-
+    repo: Repository,
 }
 
 impl Backup {
+    pub fn new(path: &Path) -> Result<Backup> {
+        let repo = Repository::open(path)?;
+        println!("Repo: {}", path.to_str().unwrap());
 
-	pub fn new() -> Backup {
+        Ok(Backup { repo: repo })
+    }
 
-		Backup{}
-	}
+    pub fn process_backup(&self) {
+        let index = self.repo.index().unwrap();
 
-	pub fn process_backup(&self) {}
-	
-	pub fn resolve_changes(&self) {}
-   	
-   	pub fn merge_files(&self) {}
-    
-    pub fn finish_backup(&self) {} 
+        match index.is_empty() {
+            true => {
+                println!("Repo clean");
+            }
+            false => {
+                println!("Repo dirty");
+                self.create_commit().unwrap();
+            }
+        }
+    }
+
+    pub fn resolve_changes(&self) {}
+
+    pub fn merge_files(&self) {}
+
+    pub fn finish_backup(&self) {}
+
+    fn get_last_commit(&self) -> Result<Commit> {
+        let obj = self.repo.head()?.resolve()?.peel(ObjectType::Commit)?;
+        Ok(obj.into_commit().unwrap())
+    }
+
+    fn create_commit(&self) -> Result<Oid> {
+        let mut index = self.repo.index()?;
+        let oid = index.write_tree()?;
+        let signature = Signature::now("Petr Hodina", "hodinapetr46@gmail.com")?;
+        let parent_commit = self.get_last_commit()?;
+        let tree = self.repo.find_tree(oid)?;
+
+        let message = "Message";
+        Ok(self.repo.commit(
+            Some("HEAD"),
+            &signature,
+            &signature,
+            message,
+            &tree,
+            &[&parent_commit],
+        )?)
+    }
 }
 
 #[cfg(test)]
 mod tests {
+
+    use super::*;
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        let backup = Backup::new(Path::new("/tmp/backup")).unwrap();
+        backup.process_backup();
     }
 }
